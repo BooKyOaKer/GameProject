7d900229e6b6af59713714f893dfdedff6f9bc44
Unknown <bookboks@hotmail.com>
2018-09-08 02:05:53 +0700

    add up down camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,40 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	public GameObject target;
-	public float followAhead;
+    public GameObject target;
+    public float followAhead;
 
-	private Vector3 targetPosition;
+    private Vector3 targetPosition;
 
-	public float smoothing;
+    public float smoothing;
 
-	public bool followTarget;
+    public bool followTarget;
 
-	// Use this for initialization
-	void Start () {
-		followTarget = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        followTarget = true;
+    }
 
-		if(followTarget)
-		{
-			targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+    // Update is called once per frame
+    void Update()
+    {
 
-			// this moves the target of the camera ahead of the player
-			if(target.transform.localScale.x > 0f)
-			{
-				targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
-			} else {
-				targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
-			}
+        if (followTarget)
+        {
+            targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
 
-			//transform.position = targetPosition;
+            // this moves the target of the camera ahead of the player
+            if (target.transform.localScale.x > 0f)
+            {
+                targetPosition = new Vector3(targetPosition.x + followAhead, targetPosition.y, targetPosition.z);
+            }
+            else
+            {
+                targetPosition = new Vector3(targetPosition.x - followAhead, targetPosition.y, targetPosition.z);
+            }
 
-			transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
-		}
-	}
+            //transform.position = targetPosition;
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        }
+    }
 }
