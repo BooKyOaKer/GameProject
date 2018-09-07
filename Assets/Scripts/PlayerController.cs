3ce8d76f3e06e03c2d32e062d40efd5ddaa1ed5d
Unknown <bookboks@hotmail.com>
2018-09-07 18:59:00 +0700

    fix bug extraJump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,180 +1,194 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float moveSpeed;
-	private float activeMoveSpeed;
+    public float moveSpeed;
+    private float activeMoveSpeed;
 
-	public bool canMove;
+    public bool canMove;
 
-	public Rigidbody2D myRigidbody;
+    public Rigidbody2D myRigidbody;
 
-	public float jumpSpeed;
+    public float jumpSpeed;
 
-	public Transform groundCheck;
-	public float groundCheckRadius;
-	public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
 
-	public bool isGrounded;
+    public bool isGrounded;
 
-	private Animator myAnim;
+    private Animator myAnim;
 
-	public Vector3 respawnPosition;
+    public Vector3 respawnPosition;
 
-	public LevelManager theLevelManager;
+    public LevelManager theLevelManager;
 
-	public GameObject stompBox;
+    public GameObject stompBox;
 
-	public float knockbackForce;
-	public float knockbackLength;
-	private float knockbackCounter;
+    public float knockbackForce;
+    public float knockbackLength;
+    private float knockbackCounter;
 
-	public float invincibilityLength;
-	private float invincibilityCounter;
+    public float invincibilityLength;
+    private float invincibilityCounter;
 
-	public AudioSource jumpSound;
-	public AudioSource hurtSound;
+    public AudioSource jumpSound;
+    public AudioSource hurtSound;
 
-	private bool onPlatform;
-	public float onPlatformSpeedModifier;
+    private bool onPlatform;
+    public float onPlatformSpeedModifier;
 
-	public float wallSlideSpeedMax = 3;
-	private bool wallSliding;
-	public GameObject body;
+    public float wallSlideSpeedMax = 3;
+    private bool wallSliding;
+    public GameObject body;
 
-	public int extraJumps;
+    public int extraJumpsValue;
+    public int extraJumps;
 
-	// Use this for initialization
-	void Start () {
-		myRigidbody = GetComponent<Rigidbody2D>();
-		myAnim = GetComponent<Animator>();
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
 
-		respawnPosition = transform.position;
+        respawnPosition = transform.position;
 
-		theLevelManager = FindObjectOfType<LevelManager>();
+        theLevelManager = FindObjectOfType<LevelManager>();
 
-		activeMoveSpeed = moveSpeed;
+        activeMoveSpeed = moveSpeed;
 
-		canMove = true;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        canMove = true;
 
-		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
 
-		if(isGrounded)
-		{
-			extraJumps = 1;
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-		if(knockbackCounter <= 0 && canMove)
-		{
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
-			if(onPlatform)
-			{
-				activeMoveSpeed = moveSpeed * onPlatformSpeedModifier;
-			} else {
-				activeMoveSpeed = moveSpeed;
-			}
+        if (isGrounded)
+        {
+            if (myRigidbody.velocity.y == 0)
+                extraJumps = extraJumpsValue;
+        }
 
-			if(Input.GetAxisRaw ("Horizontal") > 0f)
-			{
-				myRigidbody.velocity = new Vector3(activeMoveSpeed, myRigidbody.velocity.y, 0f);
-				transform.localScale = new Vector3(1f,1f,1f);
-			} else if(Input.GetAxisRaw ("Horizontal") < 0f)
-			{
-				myRigidbody.velocity = new Vector3(-activeMoveSpeed, myRigidbody.velocity.y, 0f);
-				transform.localScale = new Vector3(-1f, 1f, 1f);
-			} else {
-				myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
-			}
+        if (knockbackCounter <= 0 && canMove)
+        {
 
-			//if(Input.GetButtonDown ("Jump") && isGrounded)
-			if(Input.GetButtonDown ("Jump") && extraJumps > 0)
-			{
-				myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
-				jumpSound.Play();
-				extraJumps--;
-			}
-				
-		}
+            if (onPlatform)
+            {
+                activeMoveSpeed = moveSpeed * onPlatformSpeedModifier;
+            }
+            else
+            {
+                activeMoveSpeed = moveSpeed;
+            }
 
-		if(knockbackCounter > 0)
-		{
-			knockbackCounter -= Time.deltaTime;
+            if (Input.GetAxisRaw("Horizontal") > 0f)
+            {
+                myRigidbody.velocity = new Vector3(activeMoveSpeed, myRigidbody.velocity.y, 0f);
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0f)
+            {
+                myRigidbody.velocity = new Vector3(-activeMoveSpeed, myRigidbody.velocity.y, 0f);
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+            }
 
-			if(transform.localScale.x > 0)
-			{
-				myRigidbody.velocity = new Vector3(-knockbackForce, knockbackForce, 0f);
-			} else {
-				myRigidbody.velocity = new Vector3(knockbackForce, knockbackForce, 0f);
-			}
-		}
+            //if(Input.GetButtonDown ("Jump") && isGrounded)
+            if (Input.GetButtonDown("Jump") && extraJumps > 0)
+            {
+                extraJumps--;
+                myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+                jumpSound.Play();
+            }
 
-		if(invincibilityCounter > 0)
-		{
-			invincibilityCounter -= Time.deltaTime;
-		}
+        }
 
-		if(invincibilityCounter <= 0)
-		{
-			theLevelManager.invincible = false;
-		}
+        if (knockbackCounter > 0)
+        {
+            knockbackCounter -= Time.deltaTime;
 
-		myAnim.SetFloat("Speed", Mathf.Abs( myRigidbody.velocity.x));
-		myAnim.SetBool("Grounded", isGrounded);
+            if (transform.localScale.x > 0)
+            {
+                myRigidbody.velocity = new Vector3(-knockbackForce, knockbackForce, 0f);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector3(knockbackForce, knockbackForce, 0f);
+            }
+        }
 
-		if(myRigidbody.velocity.y < 0)
-		{
-			stompBox.SetActive(true);
-		} else {
-			stompBox.SetActive(false);
-		}
-	}
+        if (invincibilityCounter > 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+        }
 
-	public void Knockback()
-	{
-		knockbackCounter = knockbackLength;
-		invincibilityCounter = invincibilityLength;
-		theLevelManager.invincible = true;
-	}
+        if (invincibilityCounter <= 0)
+        {
+            theLevelManager.invincible = false;
+        }
+
+        myAnim.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+        myAnim.SetBool("Grounded", isGrounded);
+
+        if (myRigidbody.velocity.y < 0)
+        {
+            stompBox.SetActive(true);
+        }
+        else
+        {
+            stompBox.SetActive(false);
+        }
+    }
+
+    public void Knockback()
+    {
+        knockbackCounter = knockbackLength;
+        invincibilityCounter = invincibilityLength;
+        theLevelManager.invincible = true;
+    }
 
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.tag == "KillPlane")
-		{
-			//gameObject.SetActive(false);
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "KillPlane")
+        {
+            //gameObject.SetActive(false);
 
-			//transform.position = respawnPosition;
+            //transform.position = respawnPosition;
 
-			theLevelManager.Respawn();
-		}
+            theLevelManager.Respawn();
+        }
 
-		if(other.tag == "Checkpoint")
-		{
-			respawnPosition = other.transform.position;
-		}
-	}
+        if (other.tag == "Checkpoint")
+        {
+            respawnPosition = other.transform.position;
+        }
+    }
 
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if(other.gameObject.tag == "MovingPlatform")
-		{
-			transform.parent = other.transform;
-			onPlatform = true;
-		}
-	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = other.transform;
+            onPlatform = true;
+        }
+    }
 
-	void OnCollisionExit2D(Collision2D other)
-	{
-		if(other.gameObject.tag == "MovingPlatform")
-		{
-			transform.parent = null;
-			onPlatform = false;
-		}
-	}
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+            onPlatform = false;
+        }
+    }
 }
