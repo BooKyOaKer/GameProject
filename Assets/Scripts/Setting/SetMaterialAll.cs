6df7dc6d9fe8bcddb663a2da83c27df969cda386
Unknown <bookboks@hotmail.com>
2018-09-08 23:22:05 +0700

    add beatiful light
    still had bug && add script set all material

---
+++ Assets/Scripts/Setting/SetMaterialAll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SetMaterialAll : MonoBehaviour
+{
+
+    public Material InputMat;
+
+    // Use this for initialization
+    void Start()
+    {
+        ChangeMaterial(InputMat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeMaterial(Material newMat)
+    {
+        Renderer[] children;
+        children = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in children)
+        {
+            var mats = new Material[rend.materials.Length];
+            for (var j = 0; j < rend.materials.Length; j++)
+            {
+                mats[j] = newMat;
+            }
+            rend.materials = mats;
+        }
+    }
+}
