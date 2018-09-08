fc0f3a9023b3347dcbcef9abddcc49f18a172194
Unknown <bookboks@hotmail.com>
2018-09-08 21:25:11 +0700

    add lightaround
    No good

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,7 @@ public class CameraController : MonoBehaviour
     public float smoothing;
 
     public bool followTarget;
+    public bool fixY;
 
     // Use this for initialization
     void Start()
@@ -25,8 +26,14 @@ public class CameraController : MonoBehaviour
 
         if (followTarget)
         {
-            targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
-
+            if (fixY)
+            {
+                targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+            }
             // this moves the target of the camera ahead of the player
             if (target.transform.localScale.x > 0f)
             {
