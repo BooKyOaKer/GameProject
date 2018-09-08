97587c19212a8e18d6e598628f90a36d2bb68f28
Unknown <bookboks@hotmail.com>
2018-09-08 09:19:07 +0700

    add melee attack anime
    Press 'F' to attack

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator myAnim;
+
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    public bool isAttack;
+
+    // Use this for initialization
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnim.SetBool("Attack", isAttack);
+
+        if (timeBtwAttack <= 0)
+        {
+            isAttack = false;
+            if (Input.GetKey(KeyCode.F))
+            {
+                isAttack = true;
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    //enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+
+    }
+}
